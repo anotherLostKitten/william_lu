94d2ef3df8c7d2f7103db7f55cd79788e8bb1c13
anotherLostKitten <tpeters@stuy.edu>
2018-11-28 12:37:38 -0500

    .

--- appy.py
+++ appy.py
@@ -15,10 +15,11 @@ app = Flask(__name__)
         
 @app.route("/")
 def home():
-    return render_template("home.html", **api.weather() )
+    return render_template("home.html", **api.weather())
 
-
-
+@app.route("/info",methods=["GET"])
+def pokeinfo():
+    return render_template("info.html", **api.poke(request.form['pokemon']))
 
 if __name__ == "__main__":
 	app.debug = True
