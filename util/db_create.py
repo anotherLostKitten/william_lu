aff5f8b7452e5dcd170b92234ca6a8b3cc9c7e25
Imad <ibelkebir@stuy.edu>
2018-11-30 13:09:27 -0500

    Update database

--- util/db_create.py
+++ util/db_create.py
@@ -7,7 +7,7 @@ c = db.cursor()
 command = "CREATE TABLE data (weather TEXT, type TEXT)"
 c.execute(command)
 
-data = [["clear sky", "flying"], ["few clouds", "normal"], ["scattered clouds", "bug"], ["broken clouds", "dark"], ["shower rain", "grass"], ["drizzle", "ground"], ["rain", "water"], ["thunderstorm", "electric"], ["snow", "ice"], ["mist", "psychic"]]
+data = [["clear sky", "flying"], ["few clouds", "normal"], ["clouds", "dragon"], ["scattered clouds", "bug"], ["broken clouds", "dark"], ["shower rain", "grass"], ["drizzle", "ground"], ["rain", "water"], ["thunderstorm", "electric"], ["snow", "ice"], ["mist", "psychic"]]
 
 c.executemany("INSERT INTO data VALUES (?,?)", data)
 
