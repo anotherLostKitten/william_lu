ee5487de747ca888e8ff74be204ec590cf6a1557
Adil Gondal <agondal@stuy.edu>
2018-12-06 22:52:56 -0500

    some comments

--- util/api.py
+++ util/api.py
@@ -1,13 +1,13 @@
 # retrieves weather data, converts weather info into info about pokemon types, retrieves pokemon data
 
 from urllib import request, parse
-from urllib.error import HTTPError
-from random import sample
+from urllib.error import HTTPError #error handling for bad api call
+from random import sample #needed to get random pokemons
 import json
 import sqlite3
 
 from util.db_utils import getType
-from util.cap import capitalize
+from util.cap import capitalize #capitalizes names 
 
 def getkey(keyfile):
     f = open(keyfile, 'r')
@@ -15,24 +15,25 @@ def getkey(keyfile):
     f.close()
     return l[0]
 
-weatherkey = getkey("util/keys.txt")
+weatherkey = getkey("util/keys.txt") #gets API key 
 
+#converts weather from Kelving to Farenheit
 def convert(temp):
     return round((temp-273.15) * 1.8 + 32)
 def weather(city):
     try:
-        site= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + weatherkey
+        site= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + weatherkey 
         url = request.urlopen(site)
         dict = json.loads(url.read())
         # print (dict)
         icon = dict["weather"][0]["icon"]
-        iconUrl="http://openweathermap.org/img/w/" + icon + ".png"
+        iconUrl="http://openweathermap.org/img/w/" + icon + ".png" #gets weather icon
         input = dict['weather'][0]['main']
-        result = getType(input.lower())
-        pokelist = [poke(i['name']) for i in sample(result,9)]
+        result = getType(input.lower()) #gets pokemon type based on weather
+        pokelist = [poke(i['name']) for i in sample(result,9)] #9 random pokemons based on weather type
         for p in pokelist:
-            p[p['name']] = capitalize(p['name'])
-        content = {'img':iconUrl,'name':dict["name"],'expl':dict["weather"][0]["description"],'max' : convert(dict["main"]["temp_max"]),'min': convert(dict["main"]["temp_min"]),'now' : convert(dict["main"]["temp"]),'pokemon': pokelist}
+            p[p['name']] = capitalize(p['name']) #capitalized and removes hyphens from names
+        content = {'img':iconUrl,'name':dict["name"],'expl':dict["weather"][0]["description"],'max' : convert(dict["main"]["temp_max"]),'min': convert(dict["main"]["temp_min"]),'now' : convert(dict["main"]["temp"]),'pokemon': pokelistp} #makes app.py more readable
         return content
     except HTTPError:
         return None
